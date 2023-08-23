5718b7ba024a144c21eae88c1a88fafc19637319
kenmusili1 <kennedymusili99@gmail.com>
2023-08-23 13:51:47 -0700

    function1

--- function1_h.c
+++ function1_h.c
@@ -1,12 +1,12 @@
 #include "hshell.h"
 
 /**
- *
+ *input_analyzer - Checks the array and does the approprate action
  *@:
  *
  * Return:
  */
-void array_analy(char *buf, char **env)
+void input_analyzer(char *buf, char **env)
 {
 	char **wrd_array;
 	int accss;
@@ -24,10 +24,71 @@ void array_analy(char *buf, char **env)
 	return (0);
 }
 
-char *tokenizer(char *str, char *delim)
+/**
+ *
+ *@:
+ *
+ * Return:
+ */
+char **tokenizer(char *str, char *delim)
 {
+	int i = 0, n = 0, count = 0;
+	char **wrd = NULL;
+	char *tkn, *buf;
+
+	buf = malloc((strlen(str) + 1) * sizeof(char));
+	if (buf == NULL)
+		return (NULL);
+
+	while (str[i] != '\0')
+	{
+		if (str[i] == '\n' || str[i] == '\t' || str[i] == ' ')
+			continue
+		else
+		{
+			count++;
+			i++;
+		}
+	}
+
+	wrd = malloc((count + 1) * sizeof(char *));
+	if (wrd == NULL)
+		return (NULL);
+
+	strcpy(buf, str);
+	tkn = strtok(buf, delim);
+
+	while (tkn)
+	{
+		wrd[i] = malloc((strlen(tkn) + 1) * sizeof(char));
+		if (wrd == NULL)
+		{
+			n = i;
+			while (n >= 0)
+			{
+				free(wrd[n]);
+				n--;
+			}
+			free(wrd);
+			return (NULL);
+		}
+
+		strcpy(wrd[i], tkn);
+		tkn = strtok(NULL, delim);
+		i++;
+	}
+
+	wrd[i] = NULL;
+	free(buf);
+	return (wrd);
 }
 
+/**
+ *
+ *@:
+ *
+ * Return:
+ */
 void fork()
 {
 }

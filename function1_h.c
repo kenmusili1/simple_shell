7a6abd228e74b3a108a4c05a6fdb7963b5584aa9
LouisCR04 <louiskariuki04@gmail.com>
2023-08-23 22:43:16 +0300

    Skeleton

--- function1_h.c
+++ function1_h.c
@@ -6,7 +6,28 @@
  *
  * Return:
  */
-void (fx)
+void array_analy(char *buf, char **env)
 {
+	char **wrd_array;
+	int accss;
+
+	wrd_array = tokenizer(buf, " \t");
+	if(wrd_array == NULL)
+		perror("Error");
+
+	accss = access(wrd_array);
+	if (accss != 0)
+		perror("Error");
+	else
+		forker(wrd_array, env);
+
 	return (0);
+}
+
+char *tokenizer(char *str, char *delim)
+{
+}
+
+void fork()
+{
 }
